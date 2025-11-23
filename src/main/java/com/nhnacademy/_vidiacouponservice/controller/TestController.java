cf56f87ba93949448d60ff819a826ffcab147387
BaeJongOk <0511jongok@gmail.com>
2025-11-23 21:57:03 +0900

    fix: workflow 수정
    -

--- src/main/java/com/nhnacademy/_vidiacouponservice/controller/TestController.java
+++ src/main/java/com/nhnacademy/_vidiacouponservice/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
     @GetMapping("/test")
     public String getTestString() {
         return "ㅎㅇ?";
