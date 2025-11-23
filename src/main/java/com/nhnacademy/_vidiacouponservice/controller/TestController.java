bb701e432e98e6b9887dd7da159fd0edec4ffee8
BaeJongOk <0511jongok@gmail.com>
2025-11-23 23:42:20 +0900

    feat: test를 위한 컨트롤러 수정
    -

--- src/main/java/com/nhnacademy/_vidiacouponservice/controller/TestController.java
+++ src/main/java/com/nhnacademy/_vidiacouponservice/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.nhnacademy._vidiacouponservice.controller;
 
+import com.nhnacademy._vidiacouponservice.dto.TestResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test")
-    public String getTestString() {
-        return "ㅎㅇ?";
+    public TestResponse getTestString() {
+        return new TestResponse("ㅎㅇ?");
     }
 }
---
+++ src/main/java/com/nhnacademy/_vidiacouponservice/dto/TestResponse.java
@@ -0,0 +1,10 @@
+package com.nhnacademy._vidiacouponservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TestResponse {
+    String test;
+}
