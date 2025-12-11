348621ef5f718c7c3d3619052d9ea1a2b410205b
tjddn327 <sungwk100@gmail.com>
2025-12-11 17:58:01 +0900

    카테고리추가

---
+++ src/main/java/com/nhnacademy/_vidiacouponservice/controller/CategoryController.java
@@ -0,0 +1,22 @@
+package com.nhnacademy._vidiacouponservice.controller;
+
+import com.nhnacademy._vidiacouponservice.domain.common.KdcCategory;
+import com.nhnacademy._vidiacouponservice.domain.dto.response.KdcCategoryResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @GetMapping
+    public List<KdcCategoryResponse> getCategories() {
+        return Arrays.stream(KdcCategory.values())
+                .map(v -> new KdcCategoryResponse(v.getCode(), v.getDesc()))
+                .toList();
+    }
+}
---
+++ src/main/java/com/nhnacademy/_vidiacouponservice/domain/dto/response/KdcCategoryResponse.java
@@ -0,0 +1,6 @@
+package com.nhnacademy._vidiacouponservice.domain.dto.response;
+
+public record KdcCategoryResponse(
+        String code,
+        String name
+) {}
