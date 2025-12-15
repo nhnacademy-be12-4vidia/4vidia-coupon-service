d74728de8052a6cc910492325ecc06f31b92ced9
tjddn327 <sungwk100@gmail.com>
2025-12-15 19:57:38 +0900

    대예림

--- src/main/java/com/nhnacademy/_vidiacouponservice/domain/dto/request/CouponValidateRequest.java
+++ src/main/java/com/nhnacademy/_vidiacouponservice/domain/dto/request/CouponValidateRequest.java
@@ -3,7 +3,6 @@ package com.nhnacademy._vidiacouponservice.domain.dto.request;
 import java.util.List;
 
 public record CouponValidateRequest(
-        Long couponId,
         int amount,
         List<Long> bookIds,
         List<String> categoryKdcIds
