712e6158b7d4de749bc32d0487b24c0611c54ada
Link <mexlove2@gmail.com>
2026-03-09 14:06:37 +0900

    -

--- src/main/java/com/nhnacademy/_vidiacouponservice/controller/CategoryController.java
+++ src/main/java/com/nhnacademy/_vidiacouponservice/controller/CategoryController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RequestMapping("/categories")
 public class CategoryController {
 
+
     @GetMapping
     public List<KdcCategoryResponse> getCategories() {
         return Arrays.stream(KdcCategory.values())
